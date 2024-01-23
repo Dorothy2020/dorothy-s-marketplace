16bb871901867811b6b2d135e6f842aa2a9f8811
Dorothy2020 <wangarraakoth@gmail,com>
2024-01-23 05:46:35 +0300

    initial commit

---
+++ packages/Webkul/Payment/src/Payment/CashOnDelivery.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Webkul\Payment\Payment;
+
+use Illuminate\Support\Facades\Storage;
+
+class CashOnDelivery extends Payment
+{
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $code = 'cashondelivery';
+
+    /**
+     * Return cashondelivery redirect url
+     *
+     * @return string
+     */
+    public function getRedirectUrl()
+    {
+
+    }
+
+    /**
+     * Returns payment method image
+     *
+     * @return array
+     */
+    public function getImage()
+    {
+        $url = $this->getConfigData('image');
+
+        return $url ? Storage::url($url) : bagisto_asset('images/cash-on-delivery.png', 'shop');
+    }
+}
